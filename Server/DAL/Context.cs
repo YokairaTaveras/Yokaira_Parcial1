41487ecbed58555d302024d4da0b91592b92c7d0
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-09 21:31:57 -0400

    Creando el contexto

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Yokaira_Parcial1.Server.Models;
 
 namespace Yokaira_Parcial1.Server.DAL
 {
     public class Context : DbContext
     {
         public Context(DbContextOptions<Context> options) : base(options)
-        { }      
+        { }
+        public DbSet<Aportes> aporte { get; set; }
     }
 }
