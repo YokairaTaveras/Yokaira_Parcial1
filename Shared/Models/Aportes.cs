4796baea0df8d804c1094fa94e783e0534585132
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-09 21:30:12 -0400

    Creando el modelo

---
+++ Shared/Models/Aportes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yokaira_Parcial1.Shared.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        [StringLength(100, ErrorMessage = "Error")]
+        public string Persona { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        [StringLength(100, ErrorMessage = "Error")]
+        public string Observacion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public int Monto { get; set; }
+    }
+}
