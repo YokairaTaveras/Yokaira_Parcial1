d579417ae987862d8f1c4d17a387aca8f17a510d
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-09 21:31:36 -0400

    Creando elo registro

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Yokaira_Parcial1.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
